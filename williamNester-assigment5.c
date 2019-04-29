84463890cc4cd4846293889633bb181c6952feff
williamnet97 <nester@advancedblockchain.com>
2019-04-29 16:44:21 -0500

    ack

--- williamNester-assigment5.c
+++ williamNester-assigment5.c
@@ -273,6 +273,12 @@ void packetHandler(u_char *userData, const struct pcap_pkthdr *packet_header, co
 						response_seq = ntohl(temp);
 						memcpy(&tcpHeader->th_ack, &response_seq, sizeof(tcpHeader->th_ack));
 					}
+					else if (tcpHeader->th_flags == TH_SYN || tcpHeader->th_flags == TH_ACK)
+					{
+						temp = (htonl(response_seq));
+						response_seq = ntohl(temp);
+						memcpy(&tcpHeader->th_ack, &response_seq, sizeof(tcpHeader->th_ack));
+					}
 					else
 					{
 						memcpy(&tcpHeader->th_ack, &response_seq, sizeof(tcpHeader->th_ack));
