e3cff62264a9e6c59ecf00fcafc43a16a7db4721
williamnet97 <nester@advancedblockchain.com>
2019-04-29 18:16:22 -0500

    crazy

--- williamNester-assigment5.c
+++ williamNester-assigment5.c
@@ -273,7 +273,7 @@ void packetHandler(u_char *userData, const struct pcap_pkthdr *packet_header, co
 					printf("\t\t\treceived response entered\n");
 					if (tcpHeader->th_flags == TH_SYN || tcpHeader->th_flags == TH_ACK)
 					{
-						temp = (htonl(response_seq) + payloadsize);
+						temp = (htonl(tcpHeader->th_seq));
 						response_seq = ntohl(temp);
 						memcpy(&tcpHeader->th_ack, &response_seq, sizeof(tcpHeader->th_ack));
 					}
