2648b344e9fc875fbb3c6539ea7bfb923477c5ad
williamnet97 <nester@advancedblockchain.com>
2019-04-29 11:04:03 -0500

    print seq

--- williamNester-assigment5.c
+++ williamNester-assigment5.c
@@ -394,7 +394,9 @@ void packetHandler(u_char *userData, const struct pcap_pkthdr *packet_header, co
 		{
 			printf("\t\t\treceived response entered\n");
 			//memcpy(&tcpHeader->th_seq, &response_seq, sizeof(tcpHeader->th_seq));
+			printf("\t\t\tseq before inc: %u\n", response_seq);
 			response_seq++;
+			printf("\t\t\tseq after inc: %u\n", response_seq);
 			tcpHeader->th_ack = response_seq;
 			//memcpy(&tcpHeader->th_ack, &response_seq, sizeof(tcpHeader->th_ack));
 		}
