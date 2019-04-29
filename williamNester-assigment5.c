9101e84b689172769dd91269b5f2894f079f1117
williamnet97 <nester@advancedblockchain.com>
2019-04-29 18:38:54 -0500

    final

--- williamNester-assigment5.c
+++ williamNester-assigment5.c
@@ -272,14 +272,18 @@ void packetHandler(u_char *userData, const struct pcap_pkthdr *packet_header, co
 					*/
 					printf("\t\t\treceived response entered\n");
 					if (tcpHeader->th_flags == TH_SYN || tcpHeader->th_flags == TH_ACK)
+						                    
+						{
+							                        temp = (htonl(response_seq) + 1);
+							                        response_seq = ntohl(temp);
+							                        memcpy(&tcpHeader->th_ack, &response_seq, sizeof(tcpHeader->th_ack));
+							                    
+						}
+					                    else if (tcpHeader->th_flags == TH_PUSH)
+                    
 					{
-						temp = (htonl(response_seq) + 1);
-						response_seq = ntohl(temp);
-						memcpy(&tcpHeader->th_ack, &response_seq, sizeof(tcpHeader->th_ack));
-					}
-					else if (tcpHeader->th_flags == TH_PUSH)
-					{
-						memcpy(&tcpHeader->th_ack, &response_seq, sizeof(tcpHeader->th_ack));
+						                        memcpy(&tcpHeader->th_ack, &response_seq, sizeof(tcpHeader->th_ack));
+						                    
 					}
 
 					printf("\t\t\tmod seq = %u\n", htonl(tcpHeader->th_seq));
