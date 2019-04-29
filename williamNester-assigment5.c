1ff5b7fcc3df3901833e088488739b78aba034e6
williamnet97 <nester@advancedblockchain.com>
2019-04-29 10:29:37 -0500

    increment

--- williamNester-assigment5.c
+++ williamNester-assigment5.c
@@ -54,6 +54,7 @@ struct intf_entry ie;
 
 //response
 uint32_t response_ack, response_seq;
+uint32_t increment = 1;
 
 // functions
 void packetHandler(u_char *userData, const struct pcap_pkthdr *pkthdr, const u_char *packet);
@@ -393,7 +394,7 @@ void packetHandler(u_char *userData, const struct pcap_pkthdr *packet_header, co
 		{
 			printf("\t\t\treceived response entered\n");
 			//memcpy(&tcpHeader->th_seq, &response_seq, sizeof(tcpHeader->th_seq));
-			response_seq = response_seq + 1;
+			response_seq = response_seq + increment;
 			tcpHeader->th_ack = response_seq;
 			//memcpy(&tcpHeader->th_ack, &response_seq, sizeof(tcpHeader->th_ack));
 		}
