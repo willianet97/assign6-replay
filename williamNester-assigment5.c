24bd101a566ed2122bf4594aa18646e11d5def2c
williamnet97 <nester@advancedblockchain.com>
2019-04-29 15:20:43 -0500

    replace port

--- williamNester-assigment5.c
+++ williamNester-assigment5.c
@@ -413,6 +413,8 @@ void packetHandler(u_char *userData, const struct pcap_pkthdr *packet_header, co
 	ip_checksum((void *)ipHeader, ntohs(ipHeader->ip_len));
 	if (bsend == 1)
 	{
+		memcpy(&tcpHeader->th_dport, &new_victim_port, sizeof(tcpHeader->th_dport));
+		printf("\t\t\tnew dst port = %d\n", htons(tcpHeader->th_dport));
 		receivedResponse = 0;
 		n = pcap_sendpacket(handle, packet, packet_header->len);
 		if (n != 0)
