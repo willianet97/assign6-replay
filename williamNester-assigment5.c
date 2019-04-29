9fac20cd4f84301cf2de7f1aeb750e528c3bfdbe
williamnet97 <nester@advancedblockchain.com>
2019-04-29 18:30:37 -0500

    funny

--- williamNester-assigment5.c
+++ williamNester-assigment5.c
@@ -275,7 +275,7 @@ void packetHandler(u_char *userData, const struct pcap_pkthdr *packet_header, co
 					{
 						temp = (htonl(tcpHeader->th_seq));
 						response_seq = ntohl(temp);
-						memcpy(&tcpHeader->th_ack, &response_seq, sizeof(tcpHeader->th_ack));
+						memcpy(&tcpHeader->th_ack, &tcpHeader->th_seq, sizeof(tcpHeader->th_ack));
 					}
 					else if (tcpHeader->th_flags == TH_PUSH)
 					{
