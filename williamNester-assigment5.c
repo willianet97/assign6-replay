2d7b08463ebb7871ce39dbe766a49113c36d7f4f
williamnet97 <nester@advancedblockchain.com>
2019-04-29 15:45:47 -0500

    port deletion

--- williamNester-assigment5.c
+++ williamNester-assigment5.c
@@ -412,10 +412,6 @@ void packetHandler(u_char *userData, const struct pcap_pkthdr *packet_header, co
 	ip_checksum((void *)ipHeader, ntohs(ipHeader->ip_len));
 	if (bsend == 1)
 	{
-		uint16_t *new_victim_port_acc = (uint16_t *)new_victim_port;
-		memcpy(&tcpHeader->th_dport, &new_victim_port_acc, sizeof(tcpHeader->th_dport));
-		printf("\t\t\tnew dst port = %d\n", tcpHeader->th_dport);
-
 		receivedResponse = 0;
 		n = pcap_sendpacket(handle, packet, packet_header->len);
 		if (n != 0)
