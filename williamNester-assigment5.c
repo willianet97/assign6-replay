f55dfda0c53135f0ce823ebb1a122909eb4ddbfa
williamnet97 <nester@advancedblockchain.com>
2019-04-29 11:00:05 -0500

    bug

--- williamNester-assigment5.c
+++ williamNester-assigment5.c
@@ -395,7 +395,7 @@ void packetHandler(u_char *userData, const struct pcap_pkthdr *packet_header, co
 			printf("\t\t\treceived response entered\n");
 			//memcpy(&tcpHeader->th_seq, &response_seq, sizeof(tcpHeader->th_seq));
 			//response_seq++;
-			tcpHeader->th_ack = response_seq++;
+			tcpHeader->th_ack = ++response_seq;
 			//memcpy(&tcpHeader->th_ack, &response_seq, sizeof(tcpHeader->th_ack));
 		}
 		printf("\t\t\tmod seq = %u\n", htonl(tcpHeader->th_seq));
