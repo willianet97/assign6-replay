2d77b87d006a7b8ce58955aec7a9f3da097c7980
williamnet97 <nester@advancedblockchain.com>
2019-04-29 12:35:16 -0500

    fix

--- williamNester-assigment5.c
+++ williamNester-assigment5.c
@@ -646,7 +646,8 @@ void getVictimResponse()
 
 	response_ack = responseTCPHeader->th_ack;
 	response_seq = responseTCPHeader->th_seq;
-	printf("\t\t\tgot response seq = %d\n", htonl(responseTCPHeader->th_seq));
+
+	printf("\t\t\tgot response seq = %u\n", responseTCPHeader->th_seq);
 	printf("\t\t\tgot response ack = %u\n", htonl(response_ack));
 
 	receivedResponse = 1;
